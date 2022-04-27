3d299077be1b6dbb2dd743534aca32a8b9323e27
Francisco Cortés <daguirr_lingma@live.com>
2022-04-26 21:09:19 -0500

    nuevo2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register("sw.js");
+    navigator.serviceWorker.register("/prueba2/sw.js");
     console.log ("si funciona");
   } else {
       console.log("Aqui no funciona")
